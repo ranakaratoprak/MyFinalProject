5da27fcf92ede385914f61f183a590564e2ce9ef
ranakaratoprak <74979447+ranakaratoprak@users.noreply.github.com>
2021-03-06 16:49:14 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,41 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        //SOLID
+        //Open Closed Principle
+        //Data Transformation Object
+        static void Main(string[] args)
+        {
+            //CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            //foreach (var category in categoryManager.GetAll())
+            //{
+            //    Console.WriteLine(category.CategoryName);
+            //}
+
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+            }
+
+            //foreach (var product in productManager.GetAllByCategoryId(2))
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+            //foreach (var product in productManager.GetByUnitPrice(40,100))
+            //{
+            //    Console.WriteLine(product.ProductName);
+            //}
+
+
+        }
+    }
+}
