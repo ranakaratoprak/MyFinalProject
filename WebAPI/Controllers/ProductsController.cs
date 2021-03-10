25c83294b5f77b81ed3fe1fe96373470f55d9077
ranakaratoprak <74979447+ranakaratoprak@users.noreply.github.com>
2021-03-10 12:52:05 +0300

    11.GünSonu

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] //attribute
+    public class ProductsController : ControllerBase
+    {
+        //Loosely coupled
+        //naming convention
+        //IoC - Inversion of Control
+        IProductService _productService; //default değeri private'dır.
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()  //public List<Product> Get()
+        {
+            //Swagger
+            //Dependency chain
+            //IProductService productService = new ProductManager(new EfProductDal());
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpPost("add")]
+
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
